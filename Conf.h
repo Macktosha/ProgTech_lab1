e1a427d75f6eaf8ca72940aa28183440c9e62de0
Anton Makotra <macktosha@icloud.com>
2021-10-05 23:45:58 +0300

    Abstract class

--- Conf.h
+++ Conf.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <string>
+
+
+using namespace std;
+
 class Conf {
 private:
+	string naming;
+	string topic;
+public:
+	
+	void set_Name(string to_name);
+	void set_Topic(string ttopic);
+
+	string get_Name();
+	string get_Topic();
+
+	Conf() {}
+
+	virtual ~Conf() = default; 
+
+	Conf(string naming, string topic)
+	{
+		this->naming = naming;
+		this->topic = topic;
+	}
+
+	virtual string entire_info() = 0;
 
 };
--- ТПлаб1.cpp
+++ ТПлаб1.cpp
@@ -1,11 +1,16 @@
 ﻿#include <iostream>
 #include <fstream>// подключиться к гиту
 #include <string>
+#include "Conf.h"
 using namespace std;
 
 int main()
 {
 	
 	
+
+	
+
+		return 0;
 }
 
