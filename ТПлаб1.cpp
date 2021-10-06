d3332d57ed5c180c4bf929ae3c77bdde6a31656a
Anton Makotra <macktosha@icloud.com>
2021-10-06 16:31:59 +0300

    To remote

--- ТПлаб1.cpp
+++ ТПлаб1.cpp
@@ -1,13 +1,17 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "Conf.h"
 #include"Speaker.h"
 using namespace std;
 
 int main()
 {
+    vector<Conf> v1;
 
-		return 0;
+    Speaker S("Anton","Space","SUAI","qqqqqq");
+    v1.push_back(S);
+ 		return 0;
 }
 
